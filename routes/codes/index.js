af2dc848f8552ac8c2ea51b8fabf6389e67f863b
Hojjat Jashnniloofar <h.niloofar@gmail.com>
2019-10-10 17:06:39 +0330

    Serate Register Route

--- routes/codes/index.js
+++ routes/codes/index.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
 
 const add = require('./add');
+const register = require('./register');
 
 router.post('/', async (req, res, next) => {
   add(req, res, next);
+});
+
+router.post('/register', async (req, res, next) => {
+  register(req, res, next);
 });
 
 module.exports = router;
