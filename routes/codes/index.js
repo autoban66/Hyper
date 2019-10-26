784c2ed43bbdcb6c88041bcf2d9608570e21873a
Hojjat Jashnniloofar <h.niloofar@gmail.com>
2019-10-27 00:25:57 +0330

    Only Admin Can Add New Code

--- routes/codes/index.js
+++ routes/codes/index.js
@@ -1,21 +1,25 @@
-const passport = require('passport');
-const router = require('express').Router();
+const passport = require("passport");
+const router = require("express").Router();
 
-const add = require('./add');
-const register = require('./register');
-const count = require('./count');
+const add = require("./add");
+const register = require("./register");
+const count = require("./count");
 
-router.post('/', async (req, res, next) => {
-  add(req, res, next);
-});
+router.post(
+  "/",
+  [passport.authenticate("jwt", { session: false })],
+  async (req, res, next) => {
+    add(req, res, next);
+  }
+);
 
-router.post('/register', async (req, res, next) => {
+router.post("/register", async (req, res, next) => {
   register(req, res, next);
 });
 
 router.get(
-  '/count',
-  [passport.authenticate('jwt', { session: false })],
+  "/count",
+  [passport.authenticate("jwt", { session: false })],
   async (req, res, next) => {
     count(req, res, next);
   }
